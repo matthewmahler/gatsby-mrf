c124db6ad9d2b0439b080d23705e6288668c632a
matthewmahler <matthewjamesmahler@gmail.com>
2019-02-27 19:52:12 -0500

    typos LUL

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,9 @@ const About = () => {
       <div>
         <p>
           My Rising Fall is a pop punk/melodic hardcore band based out of
-          central NJ. Our upcoming ep "The Hate You Hold" is the result of hard
+          central NJ. Our upcoming EP "The Hate You Hold" is the result of hard
           work and experiences over the past 5 years (we wrote it in 5 days).
-          over that period of time, we have evolved our sound into what we feel
+          Over that period of time, we have evolved our sound into what we feel
           is the best representation of ourselves and our musical influences.
         </p>
       </div>
