257eedeb1c1cb314b6dec2e90922f1f5bfec9c06
matthewmahler <matthewjamesmahler@gmail.com>
2019-06-09 13:26:01 -0400

    security

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
-
 import Layout from '../components/layout'
 import Header from '../components/Header'
 import Main from '../components/Main'
